6359d102c4911c9945bb12809dee918936bfc08f
supriya-patil-ots <supriya.patil@otssolutions.com>
2022-11-16 22:23:56 -0800

    bmi-calculator-project

---
+++ projects/BMI-calculator/index.js
@@ -0,0 +1,40 @@
+const lastResults = [];
+function calculateBmi() {
+  let heightInput = document.getElementById("height").value;
+  let weightInput = document.getElementById("weight").value;
+
+  let bmiResult = weightInput / (heightInput * heightInput);
+  bmiResult = Math.round(bmiResult * 100) / 100;
+  document.getElementById("bmiResult").innerHTML = bmiResult;
+  lastResults.push(bmiResult);
+  document.getElementById("lastResult").innerHTML = lastResults;
+
+  if (heightInput <= 0 || weightInput <= 0) {
+    alert("Enter valid number");
+    document.getElementById("bmiResult").innerHTML = "";
+    document.getElementById("lastResult").innerHTML = "";
+    document.getElementById("bmiMsg").innerHTML = "";
+  }
+  //   console.log(bmiResult);
+
+  if (lastResults.length == 10) {
+    lastResults.shift();
+  }
+
+  //code for display message//
+  if (
+    (bmiResult < 18.5 && bmiResult > 0) ||
+    (bmiResult > 24.9 && bmiResult < 100)
+  ) {
+    const msg = "Awww!!! You are unfit";
+    document.getElementById("bmiMsg").innerHTML = msg;
+    document.getElementById("bmiMsg").style.color = "red";
+  } else if (bmiResult >= 18.5 && bmiResult <= 24.9) {
+    const msg = "Congratulations!!! You are fit";
+    document.getElementById("bmiMsg").innerHTML = msg;
+    document.getElementById("bmiMsg").style.color = "green";
+  }
+
+  document.getElementById("height").value = "";
+  document.getElementById("weight").value = "";
+}
